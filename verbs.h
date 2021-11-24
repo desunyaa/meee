baab69297c405310143c7873c407a904d74b4c80
desunyaa <irclyna@gmail.com>
2021-11-23 23:59:14 -0800

    Add files via upload

---
+++ verbs.h
@@ -0,0 +1,339 @@
+
+#define MAX_STR_SZ 256
+#define MAX_TRAVELABLE_ROOMS 10
+#define MAX_ROOMS 10
+#define MAX_VERBS 20
+#define MAX_NOUNS 20
+#define GOD_ID 9999
+
+
+
+
+
+//////////CREATURE
+
+struct Noun {
+	char  name[50];
+	char description[500];
+	int happy;
+	int noun_id;
+	int owner_id;
+	int health;
+	int position; 
+	int wielded; 
+	char gender[20];
+	char subject_pronoun[16]; 
+	char object_pronoun[16]; 
+	
+	char reflexive_pronoun[21]; 
+ //  char ip_pronoun[8];
+ //   char dp_pronoun[8];  
+	int alive;
+	int animate;
+	int base_cost;
+	int damage;
+	int defense;
+	int main_quest;
+
+ //  int weight;
+  // int size;
+
+
+};
+
+
+
+void create_noun(struct Noun *subject, char* name, int def_happy, int id, int helf, int pos, char* gender, char* subject_pronoun, char* object_pronoun,char* description,
+	int base_cost,  int damage,  int defense);
+
+
+void create_noun(struct Noun *subject, char* name, int def_happy, int id, int helf, int pos, char* gender, char* subject_pronoun, char* object_pronoun,char* description,
+	int base_cost,  int damage,  int defense)
+{
+
+
+
+
+
+
+	strcpy( subject->name, name);
+	subject->happy = def_happy;
+	subject->noun_id = id;
+	subject->owner_id = GOD_ID;
+	subject->wielded = 0;
+	subject->health = helf;
+	subject->main_quest = 0;
+	if (helf>0){subject->alive = 1;subject->animate = 1;}
+	if (helf==0){subject->alive = 0;subject->animate = 0;}
+	if (helf<0){subject->alive = 0;subject->animate = 1;}
+
+
+
+	subject->position = pos;
+	strcpy( subject->gender , gender);
+	strcpy( subject->subject_pronoun , subject_pronoun);
+	strcpy( subject->object_pronoun , object_pronoun);
+	strcpy( subject->description, description);
+//reflexive pronouns
+	char stringy[25];
+	strcpy( stringy, subject->object_pronoun);
+	strcat(stringy, "self");
+ //  printf("%s",stringy);
+
+	strcpy( subject->reflexive_pronoun, stringy);
+//reflexive pronouns end
+
+	subject->base_cost = base_cost;
+	subject->damage = damage;
+	subject->defense = defense;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+struct Room {
+	char  name[50];
+	char description[500];
+	int position;
+
+	int travelable_rooms[MAX_TRAVELABLE_ROOMS];
+
+
+
+
+};
+
+
+void create_room(struct Room *room, char*  name,char* description, int position, int travelable_rooms[]);
+
+
+void create_room(struct Room *room, char*  name,char* description, int position, int travelable_rooms[])
+{
+
+	
+	int jeep;
+
+
+
+	for( jeep  = 0; jeep<MAX_TRAVELABLE_ROOMS; jeep = jeep + 1){
+		room->travelable_rooms[jeep]=GOD_ID;
+	}
+	for( jeep  = 0; jeep<MAX_TRAVELABLE_ROOMS; jeep = jeep + 1){
+		room->travelable_rooms[jeep]=travelable_rooms[jeep];
+	}
+
+
+
+
+
+	strcpy( room->name, name);
+	strcpy( room->description, description);
+	room->position = position;
+
+}
+
+
+
+
+/////////////////////////////VERB FUNCTIONS
+////////////////////////////
+
+
+
+void headpat(struct Noun *subject , struct Noun *direct_object );
+void headpat(struct Noun *subject , struct Noun *direct_object )
+{
+
+
+	if((subject->position == direct_object->position) && (direct_object->animate == 0)){
+		printf("%s headpats the %s ! that was interesting .\n", subject->name, direct_object->name);
+		subject->happy = (subject->happy) + 10;
+	}
+
+	if((subject->position == direct_object->position) && (direct_object->alive == 1) && (direct_object->animate == 1)){
+		printf("%s headpats the %s ! %s blushes .\n", subject->name, direct_object->name, direct_object->subject_pronoun );
+		direct_object->happy = (direct_object->happy) + 5;
+	}
+
+	if((subject->position == direct_object->position) && (direct_object->alive == 0) && (direct_object->animate == 1)){
+		printf("%s headpats the %s's dead body ! that was not very pleasant .\n", subject->name, direct_object->name);
+		subject->happy = (subject->happy) - 10;
+	}
+	
+
+}
+
+
+
+void examine_noun(struct Noun *subject , struct Noun *direct_object );
+void examine_noun(struct Noun *subject , struct Noun *direct_object )
+{
+	if((subject->position == direct_object->position) || (subject->noun_id == direct_object->owner_id)){
+		printf(" %s \n", direct_object->description );
+
+}
+}
+
+void examine_room(struct Noun *subject , struct Room *direct_object );
+void examine_room(struct Noun *subject , struct Room *direct_object )
+{
+	if(subject->position == direct_object->position){
+		printf(" %s \n",direct_object->description );
+	}//else{printf("%s is not in the %s !\n", subject->name, direct_object->name);} removed cause of move merge
+
+
+}
+
+void move(struct Noun *subject , struct Room *direct_object,struct Room * rooms );
+void move(struct Noun *subject , struct Room *direct_object,struct Room * rooms )
+{
+	int i=0;
+	for (i=0; i<MAX_TRAVELABLE_ROOMS; i++){
+	//	printf("%d",i);
+	
+	if (subject->position == direct_object->travelable_rooms[i]){
+
+	printf("%s moves to the %s  .\n", subject->name, direct_object->name );
+	subject->position = direct_object->position;
+	printf("connections: ");
+	for(i =0; (direct_object->travelable_rooms[i] != -2) && (i<MAX_TRAVELABLE_ROOMS); i++ ){
+		
+		printf(" '%s' ",rooms[direct_object->travelable_rooms[i]].name);
+		
+		
+		
+	}
+	
+
+	}}
+	
+	
+	printf("\n");
+}
+
+
+
+
+void give_noun(struct Noun *subject, struct Noun *direct_object, struct Noun *indirect_object);
+void give_noun(struct Noun *subject, struct Noun *direct_object, struct Noun *indirect_object)
+{
+
+	if((subject->position == indirect_object->position) && (direct_object->owner_id == subject->noun_id)){
+		printf("%s gives the %s to %s .\n", subject->name, direct_object->name, indirect_object->name);
+		direct_object->owner_id = indirect_object->noun_id;
+		direct_object->wielded = 0;
+		
+		
+
+	}else{printf("You have no %s to give to the %s !\n", direct_object->name, indirect_object->name);}
+
+
+
+}
+
+
+
+void take_noun(struct Noun *subject, struct Room *room, struct Noun *direct_object);
+void take_noun(struct Noun *subject, struct Room *room, struct Noun *direct_object)
+{
+
+	if(direct_object->position == subject->position){
+		printf("%s takes the %s .\n", subject->name, direct_object->name);
+		direct_object->owner_id = subject->noun_id;
+		direct_object->position = -1;
+		
+
+	}else{printf("There is no %s in the %s !\n", direct_object->name, room->name);}
+
+
+
+}
+
+
+
+
+void drop_noun(struct Noun *subject, struct Room *room, struct Noun *direct_object);
+void drop_noun(struct Noun *subject, struct Room *room, struct Noun *direct_object)
+{
+
+	if((direct_object->owner_id == subject->noun_id)){
+		printf("%s drops the %s .\n", subject->name, direct_object->name);
+		direct_object->owner_id = GOD_ID;
+		direct_object->position = subject->position;
+		direct_object->wielded = 0;
+		
+
+	}else{printf("%s does not have a %s to drop!\n",subject->name, direct_object->name);}
+
+
+
+}
+
+
+void wield_noun(struct Noun *subject, struct Noun *direct_object);
+void wield_noun(struct Noun *subject, struct Noun *direct_object)
+{
+if((direct_object->wielded == 0) && (direct_object->owner_id == subject->noun_id)){
+		printf("%s wields the %s .\n", subject->name, direct_object->name);
+		direct_object->wielded = 1;
+		
+	
+	}else{printf("%s does not have a %s in their inventory to wield !\n",subject->name, direct_object->name);}
+
+
+
+}
+
+void unwield_noun(struct Noun *subject, struct Noun *direct_object);
+void unwield_noun(struct Noun *subject, struct Noun *direct_object)
+{
+
+if((direct_object->wielded == 1) && (direct_object->owner_id == subject->owner_id)){
+		printf("%s unwields the %s .\n", subject->name, direct_object->name);
+		direct_object->wielded = 0;
+
+	}else{printf("%s is not currently wielding a %s !\n",subject->name, direct_object->name);}
+
+
+
+}
+
+
+///add defense
+void attack(struct Noun *subject, struct Noun *direct_object,struct Noun * nouns );
+void attack(struct Noun *subject, struct Noun *direct_object,struct Noun * nouns )
+{
+
+	int damage = 1;
+	int jeep;
+
+	if(subject->position == direct_object->position){
+		for( jeep  = 0; (strcmp(nouns[jeep].name,"BUFFER NAME")); jeep = jeep + 1){
+
+			if((nouns[jeep].wielded == 1) &&(nouns[jeep].owner_id == subject->noun_id)){
+//            printf("nouns[jeep]->damage %d", nouns[jeep].damage);
+				damage = damage + nouns[jeep].damage;
+
+
+			}
+
+
+		}
+		printf("%s does %d damage to %s .\n", subject->name, damage, direct_object->name);
+		direct_object->health = direct_object->health - damage;
+		printf(" The %s now has %d health .\n", direct_object->name,  direct_object->health);
+
+
+	}else{printf("%s is not currently in the same room as %s !\n",subject->name, direct_object->name);}
+
+
+
+}
